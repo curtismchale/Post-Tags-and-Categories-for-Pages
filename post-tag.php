f63a46fe8a8c7aeb01b9d092aa2a1751d2b89ea2
curtismchale <curtis@curtismchale.ca>
2011-03-07 21:35:39 -0800

    * added tags and categories to pages * added tags to archives for posts with the same tag

--- post-tag.php
+++ post-tag.php
@@ -9,4 +9,20 @@ Author URI: http://curtismchale.ca
 License: GPLv2 or later
 */
 ?>
+<?php
 
+    // Make the metabox appear on the page editing screen
+    function ptcfp_taxonomies_for_pages() {
+        register_taxonomy_for_object_type('post_tag', 'page');
+        register_taxonomy_for_object_type('category', 'page');
+    }
+    add_action('init', 'ptcfp_taxonomies_for_pages');
+
+    // When displaying a tag archive, also show pages
+    function tags_archives($wp_query) {
+        if ( $wp_query->get('tag') )
+            $wp_query->set('post_type', 'any');
+    }
+    add_action('pre_get_posts', 'tags_archives');
+
+?>
