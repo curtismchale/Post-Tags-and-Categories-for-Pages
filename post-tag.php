6b2e9181e87642414e015732d667d371a4db678c
curtismchale <curtis@curtismchale.ca>
2011-03-07 21:25:20 -0800

    initial commit for the plugin

---
+++ post-tag.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Post Tags and Categories for Pages
+Plugin URI: 
+Description: Simply adds the stock Categories and Post Tags to your Pages..
+Version: 0.1
+Author: Curtis McHale
+Author URI: http://curtismchale.ca
+License: GPLv2 or later
+*/
+?>
+
